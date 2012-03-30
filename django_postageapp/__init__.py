82b8d49c824f1f24e37bc6776515d62e2a120d63
Edward Sherrill <teddy.sherrill@gmail.com>
2012-03-29 23:43:37 -0400

    Added file attachment capability to send_messages (currently working for a single attachment).

--- django_postageapp/__init__.py
+++ django_postageapp/__init__.py
@@ -8,51 +8,55 @@ import urllib2
 
 
 class PostageappEmailBackend(BaseEmailBackend):
-    
-    def __init__(self, fail_silently=False, **kwargs):
-        self.fail_silently = fail_silently
-        self.postageapp_endpoint = "https://api.postageapp.com/v.1.0/send_message.json"
-        
-        if not getattr(settings, "EMAIL_POSTAGEAPP_API_KEY"):
-            raise ImproperlyConfigured("You need to provide EMAIL_POSTAGEAPP_API_KEY in your Django settings file.")
-    
-    def send_messages(self, email_messages):
-        
-        if not email_messages:
-            return
-        
-        for message in email_messages:
-            
-            content = {}
-            if isinstance(message, EmailMessage):
-                content["text/%s" % message.content_subtype] = message.body
-            
-            if isinstance(message, EmailMultiAlternatives):
-                for alt in message.alternatives:
-                    content[alt[1]] = alt[0]
-            
-            message_dict = {
-                'api_key': getattr(settings, "EMAIL_POSTAGEAPP_API_KEY"),
-                'arguments': { 
-                    'recipients': message.to, 
-                    'headers': {
-                        "subject": message.subject,
-                        "from": message.from_email,
-                        "bcc": message.bcc,
-                    },
-                    'content': content,
-                } 
-            }
-            
-            if getattr(message, "extra_headers"):
-                message_dict["arguments"]["headers"].update(message.extra_headers)
-            
-            try:
-                req = urllib2.Request(self.postageapp_endpoint, simplejson.dumps(message_dict), {'Content-Type': 'application/json'})
-                response = urllib2.urlopen(req)
-            
-            except urllib2.HTTPError, e:
-                if not self.fail_silently:
-                    raise urllib2.HTTPError, e
-        
-    
+	
+	def __init__(self, fail_silently=False, **kwargs):
+		self.fail_silently = fail_silently
+		self.postageapp_endpoint = "https://api.postageapp.com/v.1.0/send_message.json"
+		
+		if not getattr(settings, "EMAIL_POSTAGEAPP_API_KEY"):
+			raise ImproperlyConfigured("You need to provide EMAIL_POSTAGEAPP_API_KEY in your Django settings file.")
+	
+	def send_messages(self, email_messages):
+		
+		if not email_messages:
+			return
+		
+		for message in email_messages:
+			
+			content = {}
+			if isinstance(message, EmailMessage):
+				content["text/%s" % message.content_subtype] = message.body
+			
+			if isinstance(message, EmailMultiAlternatives):
+				for alt in message.alternatives:
+					content[alt[1]] = alt[0]
+			
+			message_dict = {
+				'api_key': getattr(settings, "EMAIL_POSTAGEAPP_API_KEY"),
+				'arguments': { 
+					'recipients': message.to, 
+					'headers': {
+						"subject": message.subject,
+						"from": message.from_email,
+						"bcc": message.bcc,
+					},
+					'content': content,
+				} 
+			}
+			
+			if getattr(message, "extra_headers"):
+				message_dict["arguments"]["headers"].update(message.extra_headers)
+			
+			if(getattr(message, "attachments")):
+				file_name = message.attachments[0][0]
+				file_string = message.attachments[0][1].encode("base64")
+				attachment_dict = {"attachments":{file_name:{"content_type":"application/octet-stream","content":file_string}}}
+				message_dict['arguments'].update(attachment_dict)
+			
+			try:
+				req = urllib2.Request(self.postageapp_endpoint, simplejson.dumps(message_dict), {'Content-Type': 'application/json'})
+				response = urllib2.urlopen(req)
+			
+			except urllib2.HTTPError, e:
+				if not self.fail_silently:
+					raise urllib2.HTTPError, e
